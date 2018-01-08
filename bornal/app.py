c7c46d563235074a46846262e0c08feba1f964ce
Flavio Garcia <piraz@candango.org>
2018-01-08 02:53:11 -0500

    Introdued initial project structure with requirements file.
    Refs: #1

---
+++ bornal/app.py
@@ -0,0 +1,24 @@
+import gi
+
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+
+class Handler:
+
+    def onDeleteWindow(self, *args):
+        Gtk.main_quit(*args)
+
+    def onButtonPressed(self, button):
+        print(button)
+        print("Hello World!")
+
+
+builder = Gtk.Builder()
+builder.add_from_file("./glade/app_window.glade")
+builder.connect_signals(Handler())
+
+window = builder.get_object("window1")
+window.show_all()
+
+Gtk.main()
